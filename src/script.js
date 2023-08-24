c3116989bfac2c4fa49f62312930bf4e6b46f119
Mztrem7 <mweliane2013@gmail.com>
2023-08-24 12:14:48 -0300

    Segundo commit - Iniciando a api de tempo  - API Tempo

--- src/script.js
+++ src/script.js
@@ -3,13 +3,18 @@ const min = document.getElementById('low')
 const last = document.getElementById('last')
 const btn = document.getElementsByClassName('button')
 
-const API = axios.create({
+const API_BTC = axios.create({
     baseURL:'https://www.mercadobitcoin.net/api/BTC/ticker/'
 })
 
-async function consultar(){
-    const response = await API.get()
+const API_TEMPO = axios.create({
+    baseURL:'https://api.hgbrasil.com/weather?format=json-cors&key=3ba4b93b'
+});
+
+async function consultarBTC(){
+    const response = await API_BTC.get()
     max.innerText = 'Máximo :'+response.data.ticker.high;
     min.innerText = 'Mínimo :'+response.data.ticker.low;
     last.innerText = 'Atual :'+response.data.ticker.last;
 }
+
