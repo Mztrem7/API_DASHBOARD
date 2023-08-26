92c21d933aa3a0ee5eeac030ffb0136ec2664408
Mztrem7 <mweliane2013@gmail.com>
2023-08-26 19:11:27 -0300

    Adicionando a API do tempo e Arrumando o CSS

--- src/script.js
+++ src/script.js
@@ -1,14 +1,19 @@
-const max = document.getElementById('high')
-const min = document.getElementById('low')
-const last = document.getElementById('last')
-const btn = document.getElementsByClassName('button')
+let max = document.getElementById('high')
+let min = document.getElementById('low')
+let last = document.getElementById('last')
+let btn = document.getElementById('botao')
+let inpCity = document.getElementById('text')
+let btnCity = document.getElementById('cityButton')
+
+
+
 
 const API_BTC = axios.create({
     baseURL:'https://www.mercadobitcoin.net/api/BTC/ticker/'
 })
 
 const API_TEMPO = axios.create({
-    baseURL:'https://api.hgbrasil.com/weather?format=json-cors&key=3ba4b93b'
+    baseURL:'https://api.hgbrasil.com/weather?format=json-cors&key=3ba4b93b&city_name='
 });
 
 async function consultarBTC(){
@@ -18,3 +23,14 @@ async function consultarBTC(){
     last.innerText = 'Atual :'+response.data.ticker.last;
 }
 
+async function consultaTempo(){
+    let value = await inpCity.value
+    const response = await API_TEMPO.get(`${value}`)
+    console.log(response)
+    console.log(response.data.results.city)
+    console.log(response.data.results)
+}
+
+botao.addEventListener('click', consultarBTC)
+
+btnCity.addEventListener('click', consultaTempo)
